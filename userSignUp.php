266e3102e39345197f23e045e4357a3199014814
Nischal _Shakya <81230596+nischal55@users.noreply.github.com>
2024-11-07 16:58:37 +0545

    OTP verification completed

--- userSignUp.php
+++ userSignUp.php
@@ -1,15 +1,30 @@
 <?php
-if(isset($_GET['condition'])){  
+if (isset($_GET['condition'])) {
 ?>
-<script>alert("user already exists")</script>
+    <script>
+        alert("user already exists")
+    </script>
+
 <?php
 }
 
+if (isset($_GET['otp'])) {
+    ?>
+        <script>
+            alert("otp validation failed")
+        </script>
+    
+    <?php
+    }
 
+?>
+
+<?php
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -18,38 +33,42 @@ if(isset($_GET['condition'])){
     <link rel="stylesheet" href="Assets/style.css">
     <title>User | Signup</title>
 </head>
+
 <body>
     <div class="container">
-        
-            <div class="card" id="login-card">
-                <form action="backend/userSignupApi.php" method="post" style="margin-left:1rem">
+
+        <div class="card" id="login-card">
+            <form action="checkOtp.php" method="post" style="margin-left:1rem">
                 <h3 class="login-head">User Signup</h3>
-                    <label for="customer_name">Customer Name:</label><br>
-                    <input type="text" name="customer_name" id="username" required><br><br>
-                    <label for="address">Address:</label><br>
-                    <input type="text" name="address" id="password" required>
-                    <br><br>
-                    <label for="contact">Contact No:</label><br>
-                    <input type="text" name="contact" id="password" pattern="^[0-9]{10}$"required>
-                    <br><br>
-                    <label for="email">Email:</label><br>
-                    <input type="email" name="email" id="password" required>
-                    <br><br>
-                    <label for="username">Username:</label><br>
-                    <input type="text" name="username" id="password" required>
-                    <br><br>
-                    <label for="password">Password:</label><br>
-                    <input type="password" name="password" id="password" required>
-                    <br><br>
-                   
-                    
-                    <input type="submit" value="Sign Up" class="btn btn-primary" id="login-btn">
-                </form>
-            </div>
+                <label for="customer_name">Customer Name:</label><br>
+                <input type="text" name="customer_name" id="username" required><br><br>
+                <label for="address">Address:</label><br>
+                <input type="text" name="address" id="password" required>
+                <br><br>
+                <label for="contact">Contact No:</label><br>
+                <input type="text" name="contact" id="password" pattern="^[0-9]{10}$" required>
+                <br><br>
+                <label for="email">Email:</label><br>
+                <input type="email" name="email" id="password" required>
+                <br><br>
+                <label for="username">Username:</label><br>
+                <input type="text" name="username" id="password" required>
+                <br><br>
+                <label for="password">Password:</label><br>
+                <input type="password" name="password" id="password" required>
+                <br><br>
+                <input type="submit" value="Sign Up" class="btn btn-primary" id="login-btn">
         </div>
-        
+    </div>
+
+
+    </form>
+
+
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    
+
 </body>
+
 </html>
